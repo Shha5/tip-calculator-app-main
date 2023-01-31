b06acfa131d8486fbc5d4cf61490bf0cf696b288
Ewa Szczepska <ewapietrek@outlook.com>
2023-01-31 14:59:16 +0300

    trying out fix for smartphone keyboard problem

--- scripts/data-validation-style.js
+++ scripts/data-validation-style.js
@@ -1,3 +1,9 @@
+$(function() {
+    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    $("html, body").css({"width":w,"height":h});
+});
+
 //Validation functions
 function billValidation(){
 var bill = document.getElementById("bill").value;
